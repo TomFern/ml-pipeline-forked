7e5f152819f279a99a044d8ae2904e1312414356
tomas f <46322567+TomFern@users.noreply.github.com>
2023-12-08 12:45:46 -0300

    update install command

--- tools/install.sh
+++ tools/install.sh
@@ -3,5 +3,4 @@
 # This script sets up a Python virtual environment and installs the required packages for running the ML workflow scripts in the ml directory
 python3 -m venv ./venv
 source ./venv/bin/activate
-pip3 download --cache-dir .pip_cache -r requirements.txt
-pip3 install -r requirements.txt # Note that these requirements were built on Ubuntu 22.04
+pip3 install --cache-dir .pip_cache -r requirements.txt # Note that these requirements were built on Ubuntu 22.04
