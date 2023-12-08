386396170addd00f78146287e5f8131f6a42d9e5
tomas f <46322567+TomFern@users.noreply.github.com>
2023-12-08 12:57:54 -0300

    fix venv

--- tools/install.sh
+++ tools/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # This script sets up a Python virtual environment and installs the required packages for running the ML workflow scripts in the ml directory
-python3 -m venv ./venv
+python -m venv ./venv
 source ./venv/bin/activate
-pip3 install --cache-dir .pip_cache -r requirements.txt # Note that these requirements were built on Ubuntu 22.04
+pip install --cache-dir .pip_cache -r requirements.txt # Note that these requirements were built on Ubuntu 22.04
