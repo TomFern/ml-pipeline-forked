55acdb0557fc044b6d8d87ef2c3c3c0bd1c2258e
tomas f <46322567+TomFern@users.noreply.github.com>
2023-12-08 13:01:55 -0300

    deupgrade pip

--- tools/install.sh
+++ tools/install.sh
@@ -3,5 +3,4 @@
 # This script sets up a Python virtual environment and installs the required packages for running the ML workflow scripts in the ml directory
 python -m venv ./venv
 source ./venv/bin/activate
-pip install --upgrade pip
 pip install --cache-dir .pip_cache -r requirements.txt # Note that these requirements were built on Ubuntu 22.04
